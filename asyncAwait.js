6bb7b50c8c728b25fd46ad242a33d3353a4b3222
UR Rehman <umair.rehman221@gmail.com>
2026-08-29 20:03:34 +0400

    Implement async/await in getPersonsInfo function
    Added an async/await version of the getPersonsInfo function.

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,6 +1,32 @@
 // For example, the two code blocks below do the exact same thing. 
 // They both get information from a server, process it, and return a promise.
 
+const server = {
+  people: [
+    {
+      name: "Odin",
+      age: 20,
+    },
+    {
+      name: "Thor",
+      age: 35,
+    },
+    {
+      name: "Freyja",
+      age: 29,
+    },
+  ],
+
+  getPeople() {
+    return new Promise((resolve, reject) => {
+      // Simulating a delayed network call to the server
+      setTimeout(() => {
+        resolve(this.people);
+      }, 2000);
+    });
+  },
+};
+
 function getPersonsInfo(name) {
   return server.getPeople().then(people => {
     return people.find(person => { return person.name === name });
@@ -11,3 +37,5 @@ async function getPersonsInfo(name) {
   const person = people.find(person => { return person.name === name });
   return person;
 }
+
+
