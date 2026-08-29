ec2ac52bbc906959c9e72787221fad248949226a
UR Rehman <umair.rehman221@gmail.com>
2026-08-29 20:14:27 +0400

    Refactor getCats to use async/await
    Refactor getCats function to use async/await for fetching cat images.

--- asyncAwait.js
+++ asyncAwait.js
@@ -122,3 +122,20 @@ async function getPersonsInfo(name) {
       });
   }
 </script>
+
+
+  // Now that we have a function that is asynchronous, we can then start refactoring from using promises to using await:
+
+<script>
+  const img = document.querySelector('img');
+
+  async function getCats() {
+    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=YOUR_KEY_HERE&s=cats');
+    response.json().then(function(response) {
+      img.src = response.data.images.original.url;
+    }).catch(function(error) {
+      console.error(error);
+    });
+  }
+</script>
+
