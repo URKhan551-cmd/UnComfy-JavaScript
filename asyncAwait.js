a89f9cf056851cd241240138222c1460abea78e0
UR Rehman <umair.rehman221@gmail.com>
2026-08-30 14:29:04 +0400

    Implement async/await for addition with delays
    Added async/await implementation for adding numbers with delays.

--- asyncAwait.js
+++ asyncAwait.js
@@ -251,3 +251,38 @@ addPromise(10).then((sum) => {
   console.log(sum);
 });
 
+
+// here we do the same thing but now in ASYNC AWAIT paaradigm
+
+async function addAsync(x) {
+  const a = await doubleAfter2Seconds(10);
+  const b = await doubleAfter2Seconds(20);
+  const c = await doubleAfter2Seconds(30);
+  return x + a + b + c;
+}
+
+
+// await the function return value will be stored in varuable 
+
+function doubleAfter2Seconds(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x * 2);
+    }, 2000);
+  });
+}
+
+async function addAsync(x) {
+  const a = await doubleAfter2Seconds(10);
+  const b = await doubleAfter2Seconds(20);
+  const c = await doubleAfter2Seconds(30);
+  return x + a + b + c;
+}
+
+
+addAsync(10).then((sum) => {
+  console.log(sum);
+});
+
+
+// look at this code and look at the promise code a lot of confusion and boilerplate 
